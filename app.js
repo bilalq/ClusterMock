e8136b68c2631fbbe579f6eb3e15c8b2ea8b6bcf
Devon Peticolas <devon.peticolas@gmail.com>
2013-03-27 03:40:37 -0400

    cleaning

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ var iterations = 10;
 
 app.configure(function(){
   app.set('port', process.env.PORT || 4000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
