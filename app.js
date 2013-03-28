8c71dfd4f25d0b25333e4fe56a29b1a86d02172d
Devon Peticolas <devon.peticolas@gmail.com>
2013-03-28 00:00:12 -0400

    we're going semicolonless guys

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 var express = require('express')
-, http = require('http');
-
-var app = express();
-
-var intervals = {};
-var iterations = 10;
+, request = require('request')
+, http = require('http')
+, app = express()
+, iterations = 10
+, chunkSize = 100
+, intervals = {}
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 4000);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
+  app.set('port', process.env.PORT || 4000)
+  app.use(express.favicon())
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(app.router)
+})
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+  app.use(express.errorHandler())
+})
 
 app.get('/', function(req, res) {
-  console.log("someone hit index");
-  res.send("yes this api is up");
-});
+  console.log("someone hit index")
+  res.send("yes this api is up")
+})
 
 app.post('/transcode', function(req, res) {
-  console.log("transcode upload "+req.body.id);
-  var i = 0;
+  console.log("transcode upload "+req.body.id)
+  var i = 0
   if(intervals[req.body.id] === undefined){
     intervals[req.body.id] = setInterval(function() {
-      console.log("job ping", req.body.id, i);
+      console.log("job ping", req.body.id, i)
       if(i++ == iterations){
-        clearInterval(intervals[req.body.id]);
+        clearInterval(intervals[req.body.id])
       }
-    }, 3000);
-    res.send("started an interval for "+req.body.id);
+    }, 3000)
+    res.send("started an interval for "+req.body.id)
   }else{
-    res.send("interval for "+req.body.id+" defined");
+    res.send("interval for "+req.body.id+" defined")
   }
-});
+})
 
 app.del('/transcode/:id', function(req, res) {
-  clearInterval(intervals[req.params.id]);
-  delete intervals[req.params.id];
-  res.send("deleting that job with the id "+req.params.id);
-});
+  clearInterval(intervals[req.params.id])
+  delete intervals[req.params.id]
+  res.send("deleting that job with the id "+req.params.id)
+})
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+  console.log("Express server listening on port " + app.get('port'))
+})
